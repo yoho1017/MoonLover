b738d933edf9359dc8b3f11de2f9ec46a1efa03d
yoho1017 <hide1017@yahoo.com.tw>
2020-11-24 10:14:06 +0800

    上傳與更新月老地圖網頁

--- js/1_new.js
+++ js/1_new.js
@@ -29,6 +29,43 @@ $(document).ready(function(){
 
 });
 
+let vm=new Vue({
+  el:'#Taiwan',
+  data:{
+    temples:[{name:'台北龍山寺',x:'130px',y:'185px'},{name:'台中慈德慈惠堂',x:'140px',y:'380px'},{name:'台中樂成宮',x:'0px',y:'10px'},{name:'雲林北港朝天宮',x:'0px',y:'10px'},{name:'台南大天后宮',x:'0px',y:'10px'}],
+    
+  },
+  components:{
+    'my-component':{
+      props:["name"],
+      template:`
+      <ul class="line-block">
+        <li class="linkText"><a href="#">{{name}}</a></li>
+        <li class="roundLink"></li>
+        <li class="mapLine"></li>
+      </ul>`,
+    },
+  },
+  methods:{
+  },
+})
+
+ 
+// 取得座標位置
+$(document).ready(function() {
+  $('.twMap').click(function(e) {
+    var offset = $(this).offset();
+    var x = e.pageX - offset.left;
+    var y = e.pageY - offset.top
+    alert("x=" + x);
+    alert("y=" + y);
+  });
+});
+
+
+
+
+
 
 
 
