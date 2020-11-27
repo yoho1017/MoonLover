8ab6aa3bd1a4d473182853d73e48e1d53584ed8f
yoho1017 <hide1017@yahoo.com.tw>
2020-11-27 11:05:10 +0800

    修改首頁與上傳圖片

--- js/srcoll.js
+++ js/srcoll.js
@@ -22,6 +22,17 @@ var t5 = new TimelineMax({
     yoyo: true 
 });
 
+var t6 = new TimelineMax({
+    repeat : -1,
+    yoyo: true 
+});
+var t7 = new TimelineMax({
+    repeat : -1,
+    yoyo: true 
+});
+
+
+
 
 
  
@@ -30,7 +41,10 @@ t1.fromTo(['.lantern.left'],1,{rotation:10,transformOrigin:'left top',ease:Power
 t2.fromTo(['.lantern.right'],1,{rotation:-10,transformOrigin:'right top',ease:Power0.easeNone,className:"+=light"},{rotation:10,transformOrigin:'right top',ease:Power0.easeNone});
 
 t3.fromTo(['.backCloud'],4,{
-    scale:1,ease:Power0.easeNone,opacity: .6},{scale:1.5,ease:Power0.easeNone,opacity:0});
+    scale:1,ease:Power0.easeNone,opacity: .8},{scale:1.5,ease:Power0.easeNone,opacity:0});
 
 t4.fromTo(['#cloudLeft'],2,{ transformOrigin:'right bottom',rotation:0,ease:Power0.easeNone},{ rotation:10,ease:Power0.easeNone});
 t5.fromTo(['#cloudRight'],2,{ transformOrigin:'left bottom',rotation:0,ease:Power0.easeNone},{ rotation:-10,ease:Power0.easeNone});
+
+t6.fromTo(['#cloudaheadL'],2,{ transformOrigin:'right bottom',rotation:0,ease:Power0.easeNone},{ rotation:-5,ease:Power0.easeNone});
+t7.fromTo(['#cloudaheadR'],2,{ transformOrigin:'left bottom',rotation:0,ease:Power0.easeNone},{ rotation:5,ease:Power0.easeNone});
