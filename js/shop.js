c2aa56a38f2f2bbe4202047ba909ebda8fbc8658
CHENCHAN <BSChenChan1215@gmail.com>
2020-12-08 16:44:24 +0800

    新增換圖片吉祥物js,後端訂單80%完成,其他都是微調rwd

--- js/shop.js
+++ js/shop.js
@@ -1,28 +1,43 @@
 //購買商品數量
 
-let btn = document.getElementsByClassName('qty_plus');
-let btn1 = document.getElementById('minus');
-let number = document.getElementById('number');
+// let btn = document.getElementsByClassName('qty_plus');
+// let btn1 = document.getElementById('minus');
+// let number = document.getElementById('number');
 
-document.addEventListener('click', function (e) {
-    if (e.target.classList == 'qty_plus') {
-        let numberVal = e.target.closest('span').querySelector('.qty').value;
-        let total = parseInt(numberVal) + 1;
-        e.target.closest('span').querySelector('.qty').value = total;
-    }
-});
+// document.addEventListener('click', function (e) {
+//     if (e.target.classList == 'qty_plus') {
+//         let numberVal = e.target.closest('span').querySelector('.qty').value;
+//         let total = parseInt(numberVal) + 1;
+//         e.target.closest('span').querySelector('.qty').value = total;
+//     }
+// });
 
-document.addEventListener('click', function (e) {
-    let numberVal = e.target.closest('span').querySelector('.qty').value;
-    if (numberVal > 0) {
-        if (e.target.classList == 'qty_minus') {
-            let total = parseInt(numberVal) - 1;
-            e.target.closest('span').querySelector('.qty').value = total;
+// document.addEventListener('click', function (e) {
+//     let numberVal = e.target.closest('span').querySelector('.qty').value;
+//     if (numberVal > 0) {
+//         if (e.target.classList == 'qty_minus') {
+//             let total = parseInt(numberVal) - 1;
+//             e.target.closest('span').querySelector('.qty').value = total;
+//         }
+//     }
+// if (e.target.classList == 'qty_minus') {
+//     let numberVal = e.target.closest('span').querySelector('.qty').value;
+//     let total = parseInt(numberVal) - 1;
+//     e.target.closest('span').querySelector('.qty').value = total;
+// }
+// });
+
+//吉祥物換圖片-----------------------------------------------
+
+let ImgList = document.querySelectorAll('.shopPic');
+for (let i = 0; i < ImgList.length; i++) {
+    ImgList[i].addEventListener('click', function (e) {
+        console.log(ImgList[i]);
+        for (let j = 0; j < ImgList.length; j++) {
+            document.getElementsByClassName('acceptPic')[j].src = e.target.src;
         }
-    }
-    // if (e.target.classList == 'qty_minus') {
-    //     let numberVal = e.target.closest('span').querySelector('.qty').value;
-    //     let total = parseInt(numberVal) - 1;
-    //     e.target.closest('span').querySelector('.qty').value = total;
-    // }
-});
+    })
+}
+
+//吉祥物換圖片-----------------------------------------------end
+
