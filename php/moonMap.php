0bf66ab0fd39cee205615bf010c0b8bdfa2ee353
yoho1017 <hide1017@yahoo.com.tw>
2020-12-15 14:51:55 +0800

    上傳moonMap 廟宇圖片

--- php/moonMap.php
+++ php/moonMap.php
@@ -9,9 +9,9 @@
         $Util = new UtilClass();
 
         // $sql = "SELECT * FROM temple WHERE ID = ?";
-        // $sql = "SELECT t.*, l.IMAGE,l.NAME as LOC_NAME,l.GUIDE,l.OPEN_TIME,l.ADDRESS as LOC_ADDRESS FROM temple t left join temple_location l on l.lTEMPLE_ID  where t.ID=1 and l.lTEMPLE_ID = 1";
+        $sql = "SELECT t.*, l.IMAGE,l.NAME as LOC_NAME,l.GUIDE,l.OPEN_TIME,l.ADDRESS as LOC_ADDRESS FROM temple t left join temple_location l on l.lTEMPLE_ID  where t.ID=1 and l.lTEMPLE_ID = 1";
         // $sql = "SELECT t.*, l.IMAGE,l.NAME as LOC_NAME,l.GUIDE,l.OPEN_TIME,l.ADDRESS as LOC_ADDRESS FROM temple t left join temple_location l on l.lTEMPLE_ID";
-        $sql="SELECT * FROM temple WHERE ID =5";
+        // $sql="SELECT * FROM temple WHERE ID =6";
         
        
         //執行
