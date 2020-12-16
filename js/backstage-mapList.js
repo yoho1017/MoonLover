0a4de45e045f0a8c11d458a5b86046d028b4ab47
BiBiOTA <yukiotataitien@gmail.com>
2020-12-16 19:24:03 +0800

    後台除訂單管理外功能和頁面完成。 周邊景點、公版留言待串後端

--- js/backstage-mapList.js
+++ js/backstage-mapList.js
@@ -1,3 +1,14 @@
+Vue.component('paginate', VuejsPaginate);
+// 每一頁要顯示的筆數
+const PAGE_SIZE = 10;
+
+const sqlData = [
+    [1, '迪化街', '台北霞海城隍廟', '美食','好吃好吃','11:00 - 15:00','台北市迪化街'],
+    [2, '顏記杏仁露', '台北霞海城隍廟', '美食','好吃好吃好吃好吃','11:00 - 15:00','台北市迪化街'],
+    [3, '剝皮寮', '台北艋舺龍山寺', '景點','好玩好玩好玩','11:00 - 15:00','台北市萬華區'],
+    [4, '福州元祖胡椒餅', '台北艋舺龍山寺', '美食','好吃好吃好吃好吃','11:00 - 15:00','台北市萬華區']
+];
+
 var nav = new Vue({
     el: '#nav',
     data: {
@@ -9,7 +20,7 @@ var backend = new Vue({
     el: '#backend',
     data: {
         // ---------------留言清單---------------
-        username: '碩哥',
+        username: '',
         // 後台頁籤
         lists : [
             {list: '公版留言管理', href : "./backstage_publicMsg.html"},
@@ -19,17 +30,75 @@ var backend = new Vue({
             {list : '商品管理', href : "./backstage_products.html"},
             {list : '會員資料管理', href : "./backstage_member.html"},
         ],
-        sql: [
-            [1, '迪化街', '台北霞海城隍廟', '美食','好吃好吃','11:00 - 15:00','台北市迪化街'],
-            [2, '顏記杏仁露', '台北霞海城隍廟', '美食','好吃好吃好吃好吃','11:00 - 15:00','台北市迪化街'],
-            [3, '剝皮寮', '台北艋舺龍山寺', '景點','好玩好玩好玩','11:00 - 15:00','台北市萬華區'],
-            [4, '福州元祖胡椒餅', '台北艋舺龍山寺', '美食','好吃好吃好吃好吃','11:00 - 15:00','台北市萬華區'],
-            
-        ],
+        // 預設顯示頁
+        currentPage: 1,
+        // 預設頁數
+        pageCount: 1,
+        // 取得資料
+        sql: [],
+        // 修改的資料
         modify_data: '',
+        // 上傳圖片預覽(預設為false)
+        imgPreview : false,
+        // 清單顯示(預設為true)
         menu: true,
     },
+    computed: {
+        // 回傳頁數
+        pagedListdata: function () {
+            var vm = this;
+            if (vm.sql && vm.sql.length > 0) {
+                return vm.sql.filter(function (x) {
+                    return x.page === vm.currentPage;
+                })
+            }
+            else {
+                return [];
+            }
+        }
+    },
+    watch: {
+        // 呼叫頁數設定函式
+        sql: function (val) {
+            this._setPage2Model();
+        }
+    },
     methods: {
+        // 計算分頁
+        _setPage2Model: function () {
+            var vm = this;
+
+            if (!vm.sql || vm.sql.length <= 0) {
+                vm.pageCount = 1;
+            }
+            else {
+                vm.pageCount = parseInt(vm.sql.length / PAGE_SIZE) + (vm.sql.length % PAGE_SIZE > 0 ? 1 : 0);
+                for (let i = 0; i < vm.sql.length; i++) {
+                    vm.$set(vm.sql[i], "page", parseInt(i / PAGE_SIZE) + 1);
+                }
+            }
+        },
+        // 點擊分頁連結時換分頁
+        pageCallback: function (page) {
+            var vm = this;
+            this.$set(vm, 'currentPage', page);
+            // // 換分頁時卷軸到最上方
+            window.scrollTo(0,0);
+        }, 
+        upload () {
+            document.getElementById('upload').click();
+        },
+        uploadImg () {
+            var vm = this;
+            let file = document.getElementById('upload').files[0];
+            let readFile = new FileReader();
+            readFile.readAsDataURL(file);
+            // console.log(readFile);
+            readFile.addEventListener('load',function(){        
+            document.getElementById('imgPreview').src = this.result;
+            });
+            vm.imgPreview = true;
+        },       
         change(id) {
             if (id == 'n') {
                 this.modify_data[0] = this.sql.length + 1;
@@ -71,10 +140,31 @@ var backend = new Vue({
             }
         },
     },
+    created() {
+        var vm = this;
+        // 確認登入狀態
+        axios.post('./php/sessionR.php').then( response => {
+            // console.log(response);
+            data = response.data;
+            if (data != '') {
+                axios.get('./php/getMemberName.php', {
+                    params: {
+                    userId: data
+                    }
+                })
+                .then( response=> {
+                    vm.username = response.data;
+                })   
+            }
+        })        
+        // pagination套件需要從外面傳入資料才會執行。以下請串axios以後把資料傳給vm.sql(串好後上面的sqldata可刪除)
+        vm.sql = sqlData;
+    },
     mounted() {
 
-        for (i = 0; i <= this.lists.length; i++) { //取得頁面title名字來綁定頁籤class
-            if (this.lists[i] == document.title) {
+        var vm = this;
+        for (i = 0; i <= vm.lists.length -1; i++) { //取得頁面title名字來綁定頁籤class
+            if (vm.lists[i].list == document.title) {
                 $('.listName').eq(i).addClass("bold");
             }
         }
--- js/backstage-publicMsg.js
+++ js/backstage-publicMsg.js
@@ -1,3 +1,16 @@
+Vue.component('paginate', VuejsPaginate);
+// 每一頁要顯示的筆數
+const PAGE_SIZE = 10;
+
+const sqlData = [
+    [1,'F001','BiBiOTA','2020-06-06','不雅文字','這網站真的好爛啊！',1],
+    [2,'M001','Yoho','2020-11-03','不雅文字','這網站好爛！',0],
+    [3,'M003','AsoJi','2020-11-04','不雅文字','這網站好爛！',1],
+    [4,'M003','BiBiOTA','2020-11-05','不雅文字','這網站好爛！',0],
+    [5,'M003','AsoJi','2020-12-01','不雅文字','這網站好爛！',1],
+    [6,'M003','Yoho','2020-12-02','不雅文字','這網站好爛！',0],
+];
+
 var nav = new Vue ({
     el: '#nav',
     data : {
@@ -9,7 +22,7 @@ var backend = new Vue ({
     el : '#backend',
     data : {
         // ---------------留言清單---------------
-        username : '碩哥',
+        username : '',
         // 後台頁籤
         lists : [
             {list: '公版留言管理', href : "./backstage_publicMsg.html"},
@@ -19,19 +32,61 @@ var backend = new Vue ({
             {list : '商品管理', href : "./backstage_products.html"},
             {list : '會員資料管理', href : "./backstage_member.html"},
         ],
-        sql : [
-            [1,'F001','BiBiOTA','2020-06-06','不雅文字','這網站真的好爛啊！','顯示'],
-            [2,'M001','Yoho','2020-11-03','不雅文字','這網站好爛！','顯示'],
-            [3,'M003','AsoJi','2020-11-04','不雅文字','這網站好爛！','顯示'],
-            [4,'M003','BiBiOTA','2020-11-05','不雅文字','這網站好爛！','顯示'],
-            [5,'M003','AsoJi','2020-12-01','不雅文字','這網站好爛！','顯示'],
-            [6,'M003','Yoho','2020-12-02','不雅文字','這網站好爛！','顯示'],
-  
-        ],
-        modify_data : '',
-        menu : true,
+        // 預設顯示頁
+        currentPage: 1,
+        // 預設頁數
+        pageCount: 1,
+        // 取得資料
+        sql: [],
+        // 修改的資料
+        modify_data: '',
+        // 上傳圖片預覽(預設為false)
+        imgPreview : false,
+        // 清單顯示(預設為true)
+        menu: true,
+    },
+    computed: {
+        // 回傳頁數
+        pagedListdata: function () {
+            var vm = this;
+            if (vm.sql && vm.sql.length > 0) {
+                return vm.sql.filter(function (x) {
+                    return x.page === vm.currentPage;
+                })
+            }
+            else {
+                return [];
+            }
+        }
+    },
+    watch: {
+        // 呼叫頁數設定函式
+        sql: function (val) {
+            this._setPage2Model();
+        }
     },
     methods : {
+        // 計算分頁
+        _setPage2Model: function () {
+            var vm = this;
+
+            if (!vm.sql || vm.sql.length <= 0) {
+                vm.pageCount = 1;
+            }
+            else {
+                vm.pageCount = parseInt(vm.sql.length / PAGE_SIZE) + (vm.sql.length % PAGE_SIZE > 0 ? 1 : 0);
+                for (let i = 0; i < vm.sql.length; i++) {
+                    vm.$set(vm.sql[i], "page", parseInt(i / PAGE_SIZE) + 1);
+                }
+            }
+        },
+        // 點擊分頁連結時換分頁
+        pageCallback: function (page) {
+            var vm = this;
+            this.$set(vm, 'currentPage', page);
+            // // 換分頁時卷軸到最上方
+            window.scrollTo(0,0);
+        },         
         change (id) {
             if (id == 'n') {
                 this.modify_data[0] = this.sql.length +1;
@@ -68,11 +123,32 @@ var backend = new Vue ({
             }
         },
     },
+    created() {
+        var vm = this;
+        // 確認登入狀態
+        axios.post('./php/sessionR.php').then( response => {
+            // console.log(response);
+            data = response.data;
+            if (data != '') {
+                axios.get('./php/getMemberName.php', {
+                    params: {
+                    userId: data
+                    }
+                })
+                .then( response=> {
+                    vm.username = response.data;
+                })   
+            }
+        })        
+        // pagination套件需要從外面傳入資料才會執行。以下請串axios以後把資料傳給vm.sql(串好後上面的sqldata可刪除)
+        vm.sql = sqlData;
+    },
     mounted() {
 
-        for (i = 0 ; i <= this.lists.length; i++) { //取得頁面title名字來綁定頁籤class
-            if (this.lists[i] == document.title) {
-                $('.listName').eq(i).addClass( "bold" );
+        var vm = this;
+        for (i = 0; i <= vm.lists.length -1; i++) { //取得頁面title名字來綁定頁籤class
+            if (vm.lists[i].list == document.title) {
+                $('.listName').eq(i).addClass("bold");
             }
         }
     },
