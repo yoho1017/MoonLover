559afd0ebfabe4800d57705e2402612d21078ac7
yoho1017 <hide1017@yahoo.com.tw>
2020-12-17 15:09:46 +0800

    新增moonMap的美食景點連結

--- js/moonMap.js
+++ js/moonMap.js
@@ -16,7 +16,7 @@ axios.post('./php/moonMap.php', params).then( response => {
   array = []; 
   for(let i= 0; i<data.length;i++){
      
-    obj = {src: data[i].IMAGE,h3:data[i].LOC_NAME,p1:data[i].GUIDE,p2:data[i].OPEN_TIME,p3:data[i].LOC_ADDRESS,href:'https://tw.yahoo.com/'}
+    obj = {src: data[i].IMAGE,h3:data[i].LOC_NAME,p1:data[i].GUIDE,p2:data[i].OPEN_TIME,p3:data[i].LOC_ADDRESS,href:data[i].LOCATION_LINK}
     array.push(obj);
   }
   moonmap.$data.mesg = array;
--- php/moonMap.php
+++ php/moonMap.php
@@ -9,7 +9,7 @@
         $Util = new UtilClass();
 
         // $sql = "SELECT * FROM temple WHERE ID = ?";
-        $sql = "SELECT t.*, l.IMAGE,l.NAME as LOC_NAME,l.GUIDE,l.OPEN_TIME,l.ADDRESS as LOC_ADDRESS FROM temple t left join temple_location l on l.lTEMPLE_ID  where t.ID= ? and l.lTEMPLE_ID = ?";
+        $sql = "SELECT t.*, l.IMAGE,l.NAME as LOC_NAME,l.GUIDE,l.OPEN_TIME,l.ADDRESS as LOC_ADDRESS ,l.LOCATION_LINK FROM temple t left join temple_location l on l.lTEMPLE_ID  where t.ID= ? and l.lTEMPLE_ID = ?";
         // $sql = "SELECT t.*, l.IMAGE,l.NAME as LOC_NAME,l.GUIDE,l.OPEN_TIME,l.ADDRESS as LOC_ADDRESS FROM temple t left join temple_location l on l.lTEMPLE_ID";
         // $sql="SELECT * FROM temple WHERE ID =6";
         
